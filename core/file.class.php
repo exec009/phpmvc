edf7f4cd436645c236718533cfd693cea0400222
Osama Sheikh <zech@live.co.uk>
2018-08-31 14:00:03 +0500

    File Size Beauitier

--- core/file.class.php
+++ core/file.class.php
@@ -14,4 +14,14 @@ class FILE
 		$name=explode(".",$name[count($name)-1]);
 		return $name[0];
 	}
+	public static function normalizeSize(float $bytes): string
+	{
+		$units = ['B', 'KB', 'MB', 'GB', 'TB'];
+		$precision = 2;
+		$bytes = max($bytes, 0);
+		$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+		$pow = min($pow, count($units) - 1);
+		$bytes /= (1 << (10 * $pow)); 
+		return round($bytes, $precision) . ' ' . $units[$pow];
+	}
 }
